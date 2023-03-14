e933d1af44596f76c90a8bcffc4e74b9f2b93b94
nangaps <nangaparbat36@gmail.com>
2023-03-14 21:34:12 +0700

    membuat file Navbar dan membuat arrow component di app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 // import logo from './logo.svg';
 // import './App.css';
 
+const Navbar = () => {
+  return(
+    <div>
+      <h3>Hello World!</h3>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <h2>Hello World!</h2>
     </div>
   );
